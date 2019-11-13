e37af91fb9c8e96fdbe89c4f6c90ca270b6b3797
Kim Soo <k.soo@linecorp.com>
2019-11-13 11:35:23 +0900

    init and leet 997

---
+++ Google_tests/Leet997Test.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Kim Soo on 2019/11/13.
+//
+
+#include "gtest/gtest.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>> &trust) {
+        if (N == 1) {
+            return 1;
+        }
+        vector<bool> isTrusting(N + 1, false);
+        vector<int> isTrusted(N + 1, 0);
+        for (auto const &value: trust) {
+            isTrusting[value[0]] = true;
+            isTrusted[value[1]]++;
+        }
+        for (int i = 1; i < N + 1; i++) {
+            if (!isTrusting[i] && isTrusted[i] == N - 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+
+class TestFixture : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+        solution = new Solution();
+    }
+
+    virtual void TearDown() {
+        delete solution;
+    }
+
+    Solution *solution;
+};
+
+TEST_F(TestFixture, Test) {
+    vector<vector<int>> trust{{1, 2}};
+    EXPECT_EQ(solution->findJudge(2, trust), 2);
+}
